5e84f334e6e9bec2f304663a6bbfb85824ebae38
Senith Uthsara <senithkarunarathneu@gmail.com>
2022-07-25 15:13:18 +0530

    created dice 1

---
+++ game.js
@@ -0,0 +1,8 @@
+let dice1 = document.querySelector("#dice1");
+let random_number;
+
+dice1.addEventListener("click", function(){
+    random_number = Math.floor(Math.random() * 5 + 1);
+    alert(random_number);
+    dice1.setAttribute("src", "/images/dice_" + random_number+ ".png");
+});
