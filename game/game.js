601a18fc61f243765e142f4d719c4e9d08f5ec42
Senith Uthsara <senithkarunarathneu@gmail.com>
2022-07-31 02:29:21 +0530

    re-structured again

--- game/game.js
+++
@@ -1,7 +0,0 @@
-let dice1 = document.querySelector("#dice1");
-let random_number;
-
-dice1.addEventListener("click", function(){
-    random_number = Math.floor(Math.random() * 5 + 1);
-    dice1.setAttribute("src", "../images/dice_" + random_number+ ".png");
-});
